2a5552ca483ad64b26c1d68f9fb2d963dc1af04e
azambou <arthurzambou@yahoo.fr>
2018-05-24 21:58:08 +0200

    ajout classes logindata11

---
+++ src/main/java/afrique/cm/dla/zambou/snack/bean/LoginData11.java
@@ -0,0 +1,63 @@
+package afrique.cm.dla.zambou.snack.bean;
+
+public class LoginData11 {
+
+	private String name;
+	private String password;
+	private String username;
+	private String matricule;
+	private String type;
+	private String Id;
+
+	public LoginData11() {
+
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getMatricule() {
+		return  matricule;
+	}
+	
+	public void setMatricule(String matricule) {
+		this.matricule = matricule;
+	}
+
+	public String getType() {
+		return type;
+	}
+	
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	public String getId() {
+		return Id;
+	}
+	
+	public void setId(String Id) {
+		this.Id = Id;
+	}
+}
